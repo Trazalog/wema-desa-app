e735d75559107e5bb9f1aa458183f8f9f8bde250
Rogelio Sanchez <rnsanchez@trazalog.com>
2023-04-17 15:33:00 -0300

    Librería REST instanciada

--- app/Modules/wema/Controllers/Persona.php
+++ app/Modules/wema/Controllers/Persona.php
@@ -7,6 +7,12 @@ class Persona extends BaseController
 {
     public function index()
     {
-        return view('Modules\wema\Views\persona\index');
+        $url = "http://10.142.0.13:8280/services/PersonaDataService/persona";
+
+        $aux = $this->REST->callAPI("GET",$url);
+        $resp = json_decode($aux['data']);
+        $data['listadoPersonas'] = $resp;
+        var_dump($data);
+        return view('Modules\wema\Views\persona\index',$data);
     }
 }
