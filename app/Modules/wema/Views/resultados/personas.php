159d71e00c70591b71f0cb115e75197cf981473f
Rogelio Sanchez <rnsanchez@trazalog.com>
2023-06-27 11:42:16 -0300

    backup

--- app/Modules/wema/Views/resultados/personas.php
+++ app/Modules/wema/Views/resultados/personas.php
@@ -31,7 +31,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
-                    <div class="card card-light card-tabs">
+                    <div class="card card-lightblue card-tabs">
                         <div class="card-header p-0 pt-1">
                             <ul class="nav nav-tabs" id="custom-tabs-one-tab" role="tablist">
                                 <li class="nav-item">
